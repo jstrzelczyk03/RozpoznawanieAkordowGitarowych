3d1e3b1d56255162d3e775bf3da7821a4ab0d087
Jakub Strzelczyk <jstrzelczyk03@wp.pl>
2024-03-19 20:35:35 +0100

    gui

--- Sieci_neuronowe/GUI/GUI.py
+++ Sieci_neuronowe/GUI/GUI.py
@@ -1,38 +1,24 @@
-from tkinter import *
-from PIL import Image, ImageTk
+import tkinter
+import customtkinter as ctk
 from win32api import GetSystemMetrics
 
-# Funkcja do aktualizacji rozmiaru obrazu
-def resize_image(event):
-    new_width = event.width
-    new_height = event.height
-    image = original_image.resize((new_width, new_height))
-    photo = ImageTk.PhotoImage(image)
-    canvas.itemconfig(image_on_canvas, image=photo)
-    # Musimy zaktualizować referencję do obrazu, aby nie został usunięty przez garbage collector
-    canvas.image = photo
+WIDTH = int(GetSystemMetrics(0)/2)
+HEIGHT = int(GetSystemMetrics(1)/2)
 
-# Pobieranie wymiarów ekranu
-WIDTH = GetSystemMetrics(0)
-HEIGHT = GetSystemMetrics(1)
+ctk.set_default_color_theme("dark-blue")
+app = ctk.CTk()
+app.grid_columnconfigure(0, weight=1)
+app.grid_columnconfigure(1, weight=1)
+app.title("Recognizing the guitar chords")
+app.geometry(f"{WIDTH}x{HEIGHT}")
+app.minsize(WIDTH, HEIGHT)
 
-window = Tk()
-window.title("Dzejsomat - chords recognition")
-window.minsize(int(WIDTH/2), int(HEIGHT/2))
+micro = tkinter.PhotoImage(file="micro3.png")
+button = ctk.CTkButton(master=app, fg_color="blue", text="Rozpoznaj akord")
+button2 = ctk.CTkButton(master=app, fg_color="blue", text="Zagraj utwór")
 
-canvas = Canvas(window)
-canvas.grid(row=0, column=0, sticky='nsew')
 
-window.grid_rowconfigure(0, weight=1)
-window.grid_columnconfigure(0, weight=1)
+button.grid(row=0, column=0, padx=5, pady=20, sticky="ew")
+button2.grid(row=0, column=1, padx=5, pady=20, sticky="ew")
 
-# Załadowanie obrazu przy użyciu PIL
-original_image = Image.open("GuitarTloA.png")
-photo = ImageTk.PhotoImage(original_image)
-
-image_on_canvas = canvas.create_image(0, 0, anchor="nw", image=photo)
-
-# Powiązanie zdarzenia zmiany rozmiaru okna z funkcją resize_image
-window.bind('<Configure>', resize_image)
-
-window.mainloop()
+app.mainloop()
