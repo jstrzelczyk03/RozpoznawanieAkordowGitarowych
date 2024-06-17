955829133169c6f18a00c796a0eb2c3cc21f449e
Jakub Grzechnik <01178647@pw.edu.pl>
2024-06-17 16:09:55 +0200

    Final commit

--- Sieci_neuronowe/CNN.py
+++ Sieci_neuronowe/CNN.py
@@ -104,7 +104,7 @@ if __name__ == "__main__":
     model.summary()
 
     # Train the model
-    history = model.fit(X_train, y_train, validation_data=(X_validation, y_validation), batch_size=32, epochs=400)
+    history = model.fit(X_train, y_train, validation_data=(X_validation, y_validation), batch_size=32, epochs=300)
 
     # Plot accuracy and loss
     plot_history(history)
